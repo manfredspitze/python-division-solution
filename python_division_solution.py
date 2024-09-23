618b9bb7a72a6868dc2518896c496e943a509880
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-23 08:11:54 -0400

    Add files via upload

---
+++ python_division_solution.py
@@ -0,0 +1,40 @@
+# Python Division Solution
+
+# Task 1: Basic Division
+num1 = 10
+num2 = 2
+result = num1 / num2
+print(f"{num1} divided by {num2} equals {result}.")
+
+
+# Task 2: Division with Floats
+num1 = 15.50
+num2 = 4.7
+result = num1 / num2
+print(f"{num1} divided by {num2} equals {result}.")
+
+# Task 3: Divsion with User Input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter a second number: "))
+result = num1 / num2
+print(f"{num1} divided by {num2} equals {result}.")
+
+# Task 4: Calculating the average of numbers
+num1 = 10
+num2 = 20
+num3 = 30
+number_count = 3
+average = (num1 + num2 + num3) / number_count
+print(f"The average of {num1}, {num2}, and {num3} is: {average}")
+
+# Task 5: Calculating unit price
+total_cost = 25
+quantity = 5
+unit_price = total_cost / quantity
+print(f"The unit price is: ${unit_price:.2f}")
+
+# Task 6: Calculating the grade percentage
+total_points = 100
+points_earned = 85
+grade_percentage = (points_earned / total_points) * 100
+print(f"Your grade percentage is: {grade_percentage:.2f}%")
