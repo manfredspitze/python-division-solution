df2853cb53a08baa823fc8e80c53f772c6b60b1c
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-23 08:09:18 -0400

    Create python_division.py

---
+++ python_division.py
@@ -0,0 +1,48 @@
+## Python: Division in Python 
+
+### Getting Started
+
+- Create a GitHub repo named: **regular-division**
+    - Upload your project files to the repo
+    - No need to share your repo with your teacher
+    - Teacher will discuss your work with you during Teacher  Time
+
+- Create a NEW folder on your workstation desktop
+- Use VS Code to add your Python file to this new folder
+
+
+### Objective
+
+> *In this practice project, you'll learn how to use the division operator in Python.*
+
+
+### Comments, anyone?
+
+- Add a comment block (*Regular Division in Python* as your project name)
+- Add a few brief comments to explain what the main parts of your script are doing
+
+### Your Tasks
+
+- Use comments to label each task in your script
+  - Example: # Part 1, # Part 2, etc.
+- Use f-strings to display the output for each task
+
+#### Task 1
+
+Text
+
+#### Task 2
+
+Text
+
+#### Task 3
+
+Text
+
+#### Task 4
+
+Text
+
+#### Task 5
+
+Text
